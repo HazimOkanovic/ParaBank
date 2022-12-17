4b975e454a81d77ec2b8131a61d378482566159f
Hazim Okanovic <62857146+HazimOkanovic@users.noreply.github.com>
2022-12-17 19:26:43 +0100

    Feature/find transactions tests (#6)
    * added locators and methods in find transactions page
    
    * added more tests and edited the methods
    
    * added the rest of the tests and added one more method and one more locators in find transaction page

--- Pages/FindTransactionsPage.cs
+++ Pages/FindTransactionsPage.cs
@@ -6,15 +6,24 @@ namespace ParaBankPractice.Pages
     {
         private readonly By Title = By.XPath("//div//h1[@class='title']");
         private readonly By SelectAccountDropDown = By.XPath("//div//select[@id='accountId']");
+        private readonly By SelectSecondAccountFromDropdown = By.XPath("(//div//select//option)[2]");
         private readonly By TransactionIdInput = By.XPath("//div//input[@id='criteria.transactionId']");
         private readonly By DateInput = By.XPath("//div//input[@id='criteria.onDate']");
         private readonly By FromDateInput = By.XPath("//div//input[@id='criteria.fromDate']");
         private readonly By ToDateInput = By.XPath("//div//input[@id='criteria.toDate']");
         private readonly By AmountInput = By.XPath("//div//input[@id='criteria.amount']");
+        private readonly By TransactionResultAmount = By.XPath("(//td//span[contains(text(), '$')])[1]");
+        private readonly By TransactionDetails = By.XPath("(//td//a[@class = 'ng-binding'])[1]");
+        private readonly By TransactionIdNumber = By.XPath("(//tr//td)[2]");
         private readonly By FindTransactionIdButton = By.XPath("(//div/child::button)[1]");
         private readonly By FindTransactionDateButton = By.XPath("(//div/child::button)[2]");
         private readonly By FindTransactionDateRangeButton = By.XPath("(//div/child::button)[3]");
         private readonly By FindTransactionAmountButton = By.XPath("(//div/child::button)[4]");
+        private readonly By TransactionAmountFromDetails = By.XPath("//tr//td[contains(text(), '$')]");
+        private readonly By DateFormatError = By.XPath("//div//p[contains(text(), 'error')]");
+        
+        public string TransactionId { get; set; }
+        
         public FindTransactionsPage(IWebDriver driver, Enums.Enums.WebBrowser webBrowser) : base(driver, webBrowser)
         {
         }
@@ -22,6 +31,93 @@ namespace ParaBankPractice.Pages
         public string GetTitle()
         {
             return WaitElementVisibleAndGet(Title).Text;
+        }
+
+        public FindTransactionsPage SelectSecondAccount()
+        {
+            SelectFromDropdown(SelectAccountDropDown, SelectSecondAccountFromDropdown);
+            return this;
+        }
+
+        public FindTransactionsPage EnterTransactionId(string transactionId)
+        {
+            WaitElementVisibleAndGet(TransactionIdInput).SendKeys(transactionId);
+            return this;
+        }
+        
+        public FindTransactionsPage EnterTransactionDate(string transactionDate)
+        {
+            WaitElementVisibleAndGet(DateInput).SendKeys(transactionDate);
+            return this;
+        }
+        
+        public FindTransactionsPage EnterTransactionFromDate(string transactionFromDate)
+        {
+            WaitElementVisibleAndGet(FromDateInput).SendKeys(transactionFromDate);
+            return this;
+        }
+        
+        public FindTransactionsPage EnterTransactionToDate(string transactionToDate)
+        {
+            WaitElementVisibleAndGet(ToDateInput).SendKeys(transactionToDate);
+            return this;
+        }
+        
+        public FindTransactionsPage EnterTransactionAmount(string amount)
+        {
+            WaitElementVisibleAndGet(AmountInput).SendKeys(amount);
+            return this;
+        }
+
+        public string GetTransactionAmount()
+        {
+            return WaitElementVisibleAndGet(TransactionResultAmount).Text;
+        }
+        
+        public string GetTransactionAmountFromDetails()
+        {
+            return WaitElementVisibleAndGet(TransactionAmountFromDetails).Text;
+        }
+        
+        public string GetTransactionId()
+        {
+            TransactionId = WaitElementVisibleAndGet(TransactionIdNumber).Text;
+            return TransactionId;
+        }
+
+        public string GetDateFormatError()
+        {
+            return WaitElementVisibleAndGet(DateFormatError).Text;
+        }
+
+        public FindTransactionsPage ClickTransactionDetails()
+        {
+            WaitElementVisibleAndGet(TransactionDetails).Click();
+            return this;
+        }
+        
+        public FindTransactionsPage ClickFindTransactionId()
+        {
+            WaitElementVisibleAndGet(FindTransactionIdButton).Click();
+            return this;
+        }
+        
+        public FindTransactionsPage ClickFindTransactionDate()
+        {
+            WaitElementVisibleAndGet(FindTransactionDateButton).Click();
+            return this;
+        }
+        
+        public FindTransactionsPage ClickFindTransactionDateRange()
+        {
+            WaitElementVisibleAndGet(FindTransactionDateRangeButton).Click();
+            return this;
+        }
+        
+        public FindTransactionsPage ClickFindTransactionAmount()
+        {
+            WaitElementVisibleAndGet(FindTransactionAmountButton).Click();
+            return this;
         }
     }
 }
---
+++ Tests/FindTransactionTests.cs
@@ -0,0 +1,207 @@
+using System;
+using NUnit.Framework;
+using ParaBankPractice.Helpers;
+using ParaBankPractice.Pages;
+
+namespace ParaBankPractice.Tests
+{
+    public class FindTransactionTests : BaseTest
+    {
+        private readonly LogInPage logInPage;
+        private readonly HomePage homePage;
+        private readonly RegisterPage registerPage;
+        private readonly NewAccountPage newAccountPage;
+        private readonly TransferFundsPage transferFundsPage;
+        private readonly FindTransactionsPage findTransactionsPage;
+        
+        public FindTransactionTests(Enums.Enums.WebBrowser webBrowser) : base(webBrowser)
+        {
+            logInPage = new LogInPage(driver, webBrowser);
+            homePage = new HomePage(driver, webBrowser);
+            registerPage = new RegisterPage(driver, webBrowser);
+            newAccountPage = new NewAccountPage(driver, webBrowser);
+            transferFundsPage = new TransferFundsPage(driver, webBrowser);
+            findTransactionsPage = new FindTransactionsPage(driver, webBrowser);
+        }
+        
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            logInPage.GoToUrl(ConfigHelper.WEB_URL);
+        }
+
+        [Test, Order(1)]
+        public void GoToRegisterTest()
+        {
+            logInPage
+                .ClickRegisterButton();
+            
+            Assert.That(registerPage.checkSignUpTitle(), Is.EqualTo(Constants.SignUpTitle));
+        }
+
+        [Test, Order(2)]
+        public void CreateAccountTest()
+        {
+            registerPage
+                .EnterFirstName(Constants.FirstName)
+                .EnterLastName(Constants.LastName)
+                .EnterAddress(Constants.Address)
+                .EnterCity(Constants.City)
+                .EnterState(Constants.State)
+                .EnterZipCode(Constants.ZipCode)
+                .EnterPhoneNumber(Constants.PhoneNumber)
+                .EnterSsn(Constants.Ssn)
+                .EnterUserName(Constants.RandomString(7))
+                .EnterPassword(Constants.ValidPassword)
+                .EnterConfirmPassword(Constants.ValidPassword)
+                .ClickRegisterButton();
+            
+            Assert.That(homePage.GetSuccessRegistrationMessage(), Is.EqualTo(Constants.AccountCreatedMessage));
+        }
+        
+        [Test, Order(3)]
+        public void GoToNewAccountTest()
+        {
+            homePage
+                .ClickNewAccountButton();
+            
+            Assert.That(newAccountPage.GetTitle(), Is.EqualTo(Constants.NewAccountMessage));
+        }
+
+        [Test, Order(4)]
+        public void OpenNewAccountTest()
+        {
+            ThreadSleepHelper.Sleep(250);
+            
+            newAccountPage
+                .ClickOpenNewAccountButton();
+            
+            Assert.That(newAccountPage.GetSuccessMessage(), Is.EqualTo(Constants.BankAccountCreatedMessage));
+        }
+        
+        [Test, Order(5)]
+        public void GoToTransferFundsTest()
+        {
+            homePage
+                .ClickTransferFundsButton();
+            
+            Assert.That(transferFundsPage.GetTitle(), Is.EqualTo(Constants.TransferFunds));
+        }
+        
+        [Test, Order(6)]
+        public void MakeFirstTransferTest()
+        {
+            ThreadSleepHelper.Sleep(200);
+
+            transferFundsPage
+                .EnterAmount(Constants.Hundred)
+                .SelectSecondOptionToAccount()
+                .ClickTransferButton();
+            
+            ThreadSleepHelper.Sleep(500);
+            
+            Assert.That(transferFundsPage.GetTitle(), Is.EqualTo(Constants.TransferComplete));
+        }
+
+        [Test, Order(7)]
+        public void GoToFindTransactionsPage()
+        {
+            homePage
+                .ClickFindTransactionsButton();
+            
+            Assert.That(findTransactionsPage.GetTitle(), Is.EqualTo(Constants.FindTransactions));
+        }
+
+        [Test, Order(8)]
+        public void FindTransactionsByDate()
+        {
+            findTransactionsPage
+                .EnterTransactionDate(DateTime.Today.ToString("M-d-yyyy"))
+                .ClickFindTransactionDate();
+            
+            ThreadSleepHelper.Sleep(300);
+            
+            Assert.That(findTransactionsPage.GetTitle(), Is.EqualTo(Constants.TransactionResults));
+            Assert.That(findTransactionsPage.GetTransactionAmount(), Is.EqualTo(Constants.OneHundredDollars));
+        }
+
+        [Test, Order(9)]
+        public void GetTransactionIdAndCheckAmount()
+        {
+            findTransactionsPage
+                .ClickTransactionDetails()
+                .GetTransactionId();
+            
+            Assert.That(findTransactionsPage.GetTitle(), Is.EqualTo(Constants.TransactionDetails));
+            Assert.That(findTransactionsPage.GetTransactionAmountFromDetails(), Is.EqualTo(Constants.OneHundredDollars));
+        }
+        
+        [Test, Order(10)]
+        public void GoToTransactionPageAndFindById()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionId(findTransactionsPage.TransactionId)
+                .ClickFindTransactionId();
+            
+            Assert.That(findTransactionsPage.GetTransactionAmount(), Is.EqualTo(Constants.OneHundredDollars));
+        }
+        
+        [Test, Order(11)]
+        public void GoToTransactionPageAndFindByDateRange()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionFromDate(DateTime.Today.AddDays(-1).ToString("M-d-yyyy"))
+                .EnterTransactionToDate(DateTime.Today.AddDays(1).ToString("M-d-yyyy"))
+                .ClickFindTransactionDateRange();
+            
+            Assert.That(findTransactionsPage.GetTransactionAmount(), Is.EqualTo(Constants.OneHundredDollars));
+        }
+        
+        [Test, Order(12)]
+        public void GoToTransactionPageAndFindByAmount()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionAmount(Constants.Hundred)
+                .ClickFindTransactionAmount();
+            
+            Assert.That(findTransactionsPage.GetTransactionAmount(), Is.EqualTo(Constants.OneHundredDollars));
+        }
+        
+        [Test, Order(13)]
+        public void IncorrectDateFormatTest()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionDate(DateTime.Today.ToString("M/d/yyyy"))
+                .ClickFindTransactionDate();
+            
+            Assert.That(findTransactionsPage.GetDateFormatError(), Is.EqualTo(Constants.InternalError));
+        }
+        
+        [Test, Order(14)]
+        public void DateRangeIncorrectFormatTest()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionFromDate(DateTime.Today.AddDays(-1).ToString("M/d/yyyy"))
+                .EnterTransactionToDate(DateTime.Today.AddDays(1).ToString("M/d/yyyy"))
+                .ClickFindTransactionDateRange();
+            
+            Assert.That(findTransactionsPage.GetDateFormatError(), Is.EqualTo(Constants.InternalError));
+        }
+
+        [Test, Order(15)]
+        public void IncorrectIdNumberTest()
+        {
+            homePage
+                .ClickFindTransactionsButton()
+                .EnterTransactionId(Constants.Ssn)
+                .ClickFindTransactionId();
+            
+            Assert.That(findTransactionsPage.GetDateFormatError(), Is.EqualTo(Constants.InternalError));
+        }
+    }
+}
