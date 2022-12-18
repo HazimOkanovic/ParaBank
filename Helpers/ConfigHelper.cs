44ff6c56093e1dbf0332e981c3177a6476a2edc7
Hazim Okanovic <hazimokanovic@live.com>
2022-12-18 21:18:47 +0100

    updated project setup and configuration

--- Helpers/ConfigHelper.cs
+++ Helpers/ConfigHelper.cs
@@ -6,34 +6,11 @@ namespace ParaBankPractice.Helpers
 {
     public class ConfigHelper
     {
-        public static string WEB_ENV { get; }
         public static string WEB_URL { get; }
-        public static string WEB_API_URL { get; }
 
         static ConfigHelper()
         {
-            WEB_ENV = ReadConfiguration("env");
             WEB_URL = ReadConfiguration("url");
-            WEB_API_URL = ReadConfiguration("apiUrl");
-        }
-
-        private static bool ReadBooleanConfiguration(string key)
-        {
-            string rawValue = "";
-            bool value;
-            try
-            {
-                rawValue = ReadConfiguration(key);
-                value = Boolean.Parse(rawValue);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Configuration parameter: '" + key +
-                                    "' should be Boolean and have value of true/false, but the value was: '" +
-                                    rawValue + "'");
-            }
-
-            return value;
         }
 
         private static string ReadConfiguration(string key)
@@ -47,12 +24,6 @@ namespace ParaBankPractice.Helpers
             {
                 Console.WriteLine("Reading configuration for key: " + key + " from .config");
                 value = ConfigurationManager.AppSettings.Get(key);
-            }
-
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                Console.WriteLine("Reading configuration for key: " + key + " from environment variable");
-                value = Environment.GetEnvironmentVariable(key);
             }
 
             if (string.IsNullOrWhiteSpace(value))
